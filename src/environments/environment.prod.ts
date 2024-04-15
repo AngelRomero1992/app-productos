40a9d65c975fceb3be9b9c6cd23ea7a4da384238
Angel Romero <hernandezlito4@gmail.com>
2024-04-15 15:05:20 -0600

    primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBQ2WA56dSV_5D0m6k6h9tx5swgVouy8Kc",
+    authDomain: "product-admin-app-20c5e.firebaseapp.com",
+    projectId: "product-admin-app-20c5e",
+    storageBucket: "product-admin-app-20c5e.appspot.com",
+    messagingSenderId: "267331557042",
+    appId: "1:267331557042:web:d319cef6da690e56211bab"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBQ2WA56dSV_5D0m6k6h9tx5swgVouy8Kc",
+    authDomain: "product-admin-app-20c5e.firebaseapp.com",
+    projectId: "product-admin-app-20c5e",
+    storageBucket: "product-admin-app-20c5e.appspot.com",
+    messagingSenderId: "267331557042",
+    appId: "1:267331557042:web:d319cef6da690e56211bab"
+  }
 };
 
 /*
